c3531109296408fd4244aef955ff66cafec39e1b
moosemorse <aivaylovivanov@gmail.com>
2023-08-25 19:04:52 +0100

    submit/clear button working

--- GUI.py
+++ GUI.py
@@ -7,16 +7,27 @@ root = Tk()
 root.geometry("960x540")
 root.title("AI Text Detector")
 
+textArea = Text(height = 15, width = 60, font = ("arial", 10))  
 
-heading = Label(text = "AI TEXT DETECTOR", font = ("Monospace", 30))
-submitBtn = Button(text = "Submit", bg = "#04AA6D" )
-textArea = Text(height = 10, width = 50, font = ("arial", 10)) 
-percentage = Label(text = percent, font = ("Monospace", 30))
+def submit(): 
+    print(textArea.get("1.0", "end-1c")) #placeholder before we implement model 
+
+def clearText(): 
+    textArea.delete("1.0", "end") 
+
+heading = Label(text = "AI TEXT DETECTOR", font = "Monospace 30 bold")
+examples = Label()
+submitBtn = Button(text = "Submit", bg = "#04AA6D", height = 2, width = 10, command=submit)
+clearBtn = Button(text = "Clear", height = 2, width = 10, command =clearText)
+percentage = Label(text = str(percent) + " %", font = ("Monospace", 30))
+
+
 
 #place widgets on top of eachother 
 heading.pack()
-textArea.pack() 
+textArea.pack()
+clearBtn.pack()
 submitBtn.pack()
-percentage.pack() 
+percentage.pack()
 #code keeps displaying - keeps window open 
 mainloop() 
