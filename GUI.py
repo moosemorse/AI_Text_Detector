978abc91c00148b3361551fdb99169f6a5a2f987
moosemorse <aivaylovivanov@gmail.com>
2023-08-25 16:38:39 +0100

    hello world window

--- GUI.py
+++ GUI.py
@@ -1,4 +1,15 @@
-import tkinter as tk 
+from tkinter import *
 
-window = tk.Tk() 
-print("Hello World!") 
+#create root window 
+root = Tk()
+#dimensions of window and title
+root.geometry("960x540")
+root.title("AI Text Detector")
+
+
+heading = Label(text = "AI TEXT DETECTOR", font = ("Monospace", 30))
+
+
+#code keeps displaying - keeps window open 
+heading.pack()
+mainloop() 
